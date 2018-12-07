e6d37817295bc36e0ff364a0c08b619fab2ec6bc
bart.malfait@oz.be <bart.malfait@oz.be>
2018-12-07 07:25:07 +0100

    interface for controller

---
+++ SwitchFully.IntakeApp.API/Interfaces/IController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwitchFully.IntakeApp.API.Interfaces
+{
+	public interface IController<T, U>
+	{
+		ActionResult GetAll();
+		ActionResult<U> GetById(int id);
+		ActionResult<U> Update(T objectToUpdate);
+		ActionResult<U> Create(T objectToCreate);
+	}
+}
