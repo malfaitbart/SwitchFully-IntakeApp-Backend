565e8d9153790f45b8db3874fbe5a9f7ec74144e
WillemPolfliet <willem.polfliet@cegeka.com>
2018-12-12 13:05:05 +0100

    candidate integration tests

--- SwitchFully.IntakeApp.API/Candidates/DTO/CandidateDtoWithoutId.cs
+++ SwitchFully.IntakeApp.API/Candidates/DTO/CandidateDtoWithoutId.cs
@@ -10,6 +10,5 @@ namespace SwitchFully.IntakeApp.API.Candidates.DTO
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
 		public string Email { get; set; }
-
 	}
 }
--- SwitchFully.IntakeApp.Tests/SwitchFully.IntakeApp.Tests.Integration.Tests/Candidates/CandidateIntegrationTest.cs
+++ SwitchFully.IntakeApp.Tests/SwitchFully.IntakeApp.Tests.Integration.Tests/Candidates/CandidateIntegrationTest.cs
@@ -20,13 +20,8 @@ namespace SwitchFully.IntakeApp.Integration.Tests.Candidates
     public class CandidateIntegrationTest
     {
         private readonly HttpClient _client;
-        CandidateDto campaignToCreate =
-            new CandidateDto {
-                Email = "test@test.test",
-                FirstName = "test",
-                LastName = "test",
-                Id = Guid.NewGuid().ToString()
-            };
+        CandidateDtoWithoutId candidateToCreate = new CandidateDtoWithoutId { Email = "test@test.test", FirstName = "test", LastName = "test"};
+
 
 
         public CandidateIntegrationTest()
@@ -41,10 +36,79 @@ namespace SwitchFully.IntakeApp.Integration.Tests.Candidates
                 ))
                 .CreateClient();
         }
-              
 
-      
-        
+        private void AssertCampaignIsEqual(CandidateDto candidateToCompare, CandidateDtoWithoutId candidateReference)
+        {
+            Assert.Equal(candidateToCompare.Email, candidateReference.Email);
+            Assert.Equal(candidateToCompare.LastName, candidateReference.LastName);
+            Assert.Equal(candidateToCompare.FirstName, candidateReference.FirstName);
+        }
+
+        private async Task<HttpResponseMessage> PostNewCandidate()
+        {
+            var content = JsonConvert.SerializeObject(candidateToCreate);
+            var stringContent = new StringContent(content, Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync("/api/candidates", stringContent);
+            response.EnsureSuccessStatusCode();
+            return response;
+        }
+
+
+
+        [Fact]
+        public async Task CreateNewCandidate()
+        {
+            _client.DefaultRequestHeaders.Add(AuthenticatedTestRequestMiddleware.TestingHeader, AuthenticatedTestRequestMiddleware.TestingHeaderValue);
+            HttpResponseMessage response = await PostNewCandidate();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var createdCandidate = JsonConvert.DeserializeObject<CandidateDto>(responseString);
+
+            AssertCampaignIsEqual(createdCandidate, candidateToCreate);
+            Assert.True(createdCandidate.Id != null);
+        }
+
+        [Fact]
+        public async Task GetAllCandidate()
+        {
+            _client.DefaultRequestHeaders.Add(AuthenticatedTestRequestMiddleware.TestingHeader, AuthenticatedTestRequestMiddleware.TestingHeaderValue);
+
+            HttpResponseMessage responseCreate1 = await PostNewCandidate();
+            HttpResponseMessage responseCreate2 = await PostNewCandidate();
+            HttpResponseMessage responseCreate3 = await PostNewCandidate();
+
+
+            var response = await _client.GetAsync("/api/candidates");
+            response.EnsureSuccessStatusCode();
+
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var allCampaigns = JsonConvert.DeserializeObject<IEnumerable<CandidateDto>>(responseString);
+
+            Assert.NotEmpty(allCampaigns);
+        }
+
+        [Fact]
+        public async Task GetSingleCandidate()
+        {
+
+            _client.DefaultRequestHeaders.Add(AuthenticatedTestRequestMiddleware.TestingHeader, AuthenticatedTestRequestMiddleware.TestingHeaderValue);
+            HttpResponseMessage responseCreate = await PostNewCandidate();
+
+
+            var responseString = await responseCreate.Content.ReadAsStringAsync();
+            var createdCandidate = JsonConvert.DeserializeObject<CandidateDto>(responseString);
+            var responseReturn = await _client.GetAsync("/api/candidates/" + createdCandidate.Id.ToString());
+            responseReturn.EnsureSuccessStatusCode();
+
+
+            var responseStringReturn = await responseReturn.Content.ReadAsStringAsync();
+            var singlecandidate = JsonConvert.DeserializeObject<CandidateDto>(responseString);
+
+            Assert.Equal(createdCandidate.Id.ToString(), singlecandidate.Id.ToString());
+        }
+
 
     }
 }
